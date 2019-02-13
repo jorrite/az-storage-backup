05df8e4dd726614d72097f88d6a65c91dff9e381
Jorrit Elfferich <jorrit@elfferi.ch>
2019-02-13 13:53:35 +0100

    removing slash because not needed

--- backup.sh
+++ backup.sh
@@ -3,6 +3,6 @@ BACKUP_CONTAINER="backup-`date +%Y-%m-%d-%H-%M`"
 
 azcopy --source $SOURCE \
     --source-key $SOURCE_KEY \
-    --destination $DESTINATION/$DESTINATION_CONTAINER_PREFIX-$BACKUP_CONTAINER \
+    --destination $DESTINATION$DESTINATION_CONTAINER_PREFIX-$BACKUP_CONTAINER \
     --dest-key $DESTINATION_KEY \
     --recursive
