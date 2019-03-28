871b60cf15dcfe378946105f31b8d9ae472cfb8f
Jorrit Elfferich <jorrit@elfferi.ch>
2019-03-28 19:02:20 +0100

    complete rewrite to make sure containers exist too..

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,21 @@
-FROM microsoft/dotnet:2.1-runtime-alpine
+FROM debian:jessie
 
-RUN apk add --no-cache curl bash rsync
+RUN apt-get update \
+    && apt-get install curl apt-transport-https lsb-release rsync gnupg libunwind-dev libicu-dev -y
 
-RUN curl -L -o azcopy.tar.gz \
-    https://aka.ms/downloadazcopyprlinux \
-    && tar -xf azcopy.tar.gz && rm -f azcopy.tar.gz \
-    && ./install.sh && rm -f install.sh \
-    && rm -rf azcopy
+RUN curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
+    gpg --dearmor | \
+    tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+RUN AZ_REPO=$(lsb_release -cs) \
+    && echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    tee /etc/apt/sources.list.d/azure-cli.list
+
+RUN apt-get update && apt-get install azure-cli -y
+RUN mkdir azcopy \
+    && curl -L -o azcopy.tar.gz \
+    https://aka.ms/downloadazcopy-v10-linux \
+    && tar -xf azcopy.tar.gz -C azcopy --strip-components=1 && rm -f azcopy.tar.gz \
+    && ln -s /azcopy/azcopy /usr/bin/azcopy
 
 COPY . .
 RUN chmod u+x backup.sh
--- backup.sh
+++ backup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-BACKUP_CONTAINER="backup-`date +%Y-%m-%d-%H-%M`"
+BACKUP_NAME="backup-`date +%Y-%m-%d-%H-%M`"
+end=`date -d "30 minutes" '+%Y-%m-%dT%H:%M:%SZ'`
 
-azcopy --source $SOURCE \
-    --source-key $SOURCE_KEY \
-    --destination $DESTINATION$DESTINATION_CONTAINER_PREFIX-$BACKUP_CONTAINER \
-    --dest-key $DESTINATION_KEY \
-    --recursive
+#make sure source container exists
+az storage container create -n $SOURCE_CONTAINER --account-key $SOURCE_KEY --account-name $SOURCE_NAME
+#make sure dest container exists
+az storage container create -n $SOURCE_CONTAINER-$BACKUP_NAME --account-key $DESTINATION_KEY --account-name $DESTINATION_NAME
+
+SOURCE_SAS=$(az storage container generate-sas -n $SOURCE_CONTAINER --account-key $SOURCE_KEY --account-name $SOURCE_NAME --https-only --permissions dlrw --expiry $end -otsv)
+DESTINATION_SAS=$(az storage container generate-sas -n $SOURCE_CONTAINER-$BACKUP_NAME --account-key $DESTINATION_KEY --account-name $DESTINATION_NAME --https-only --permissions dlrw --expiry $end -otsv)
+azcopy cp "https://$SOURCE_NAME.blob.core.windows.net/$SOURCE_CONTAINER?$SOURCE_SAS" "https://$DESTINATION_NAME.blob.core.windows.net/$SOURCE_CONTAINER-$BACKUP_NAME?$DESTINATION_SAS" --recursive=true
