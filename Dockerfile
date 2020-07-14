b71d044f963cb6f7b2a134e82a3064d424383df8
Jorrit Elfferich <jorrit@elfferi.ch>
2020-07-14 12:23:06 +0200

    restructuring a bit:
    - Dockerfile now builds pinned version of AzCopy from source
    
    - Adding in tests
    
    - Add ignore file
    
    - Added support for SYNC. Sync only works with a 1:1 relation between source and destination SA, though. It is an option to completely mirror a source SA to dest SA. FULL mode is just regular old backup that can backup multiple source SA's to one destination SA. Due to this nature, everything is stored to blobs at the destination so you do not hit FileShare limits (5TB max quota).

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,20 @@
-FROM mcr.microsoft.com/azure-cli:2.7.0
+FROM golang:alpine as builder
 
+RUN apk add --no-cache git && rm -rf /var/cache/apk/*
+RUN go get -u github.com/Azure/azure-storage-azcopy
+WORKDIR /go/src/github.com/Azure/azure-storage-azcopy
+RUN git checkout tags/v10.5.0
+ENV GOOS linux
+ENV GARCH amd64
+ENV CGO_ENABLED 0
+RUN go install -v -a -installsuffix cgo
+
+FROM mcr.microsoft.com/azure-cli:2.8.0
+
+COPY --from=builder /go/bin/azure-storage-azcopy /usr/local/bin/azcopy
 RUN apk add --no-cache tini jq bind
 RUN az extension add --name storage-preview
-RUN chmod +x /root/.azure/cliextensions/storage-preview/azext_storage_preview/azcopy/azcopy_linux_amd64_10.3.1/azcopy
-RUN ln -s /root/.azure/cliextensions/storage-preview/azext_storage_preview/azcopy/azcopy_linux_amd64_10.3.1/azcopy /usr/local/bin/azcopy
 
-RUN mkdir /app
 WORKDIR /app
 
 COPY backup.sh .
@@ -16,6 +25,8 @@ ENV SOURCE_STORAGE_ACCOUNT_KEY ""
 ENV DESTINATION_STORAGE_ACCOUNT_NAME ""
 ENV DESTINATION_STORAGE_ACCOUNT_KEY ""
 ENV BACKUP_RETENTION_COUNT ""
+# mode can be FULL or SYNC
+ENV MODE "" 
 
 ENTRYPOINT ["/sbin/tini", "--"]
 CMD ["/app/backup.sh"]
--- backup.sh
+++ backup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+[ -z "$SOURCE_STORAGE_ACCOUNT_NAME" ] && echo -e "\e[31m\$SOURCE_STORAGE_ACCOUNT_NAME is empty\e[0m" && exit 1
+[ -z "$SOURCE_STORAGE_ACCOUNT_KEY" ] && echo "e[31m\$SOURCE_STORAGE_ACCOUNT_KEY is empty\e[0m" && exit 1
+[ -z "$DESTINATION_STORAGE_ACCOUNT_NAME" ] && echo "e[31m\$DESTINATION_STORAGE_ACCOUNT_NAME is empty\e[0m" && exit 1
+[ -z "$DESTINATION_STORAGE_ACCOUNT_KEY" ] && echo "e[31m\$DESTINATION_STORAGE_ACCOUNT_KEY is empty\e[0m" && exit 1
+[ "$MODE" != "FULL" ] && [ "$MODE" != "SYNC" ] && echo "e[31m\$MODE should be 'FULL' or 'SYNC'\e[0m" && exit 1
+
 blob_enabled=$(dig +short $SOURCE_STORAGE_ACCOUNT_NAME.blob.core.windows.net)
 file_enabled=$(dig +short $SOURCE_STORAGE_ACCOUNT_NAME.file.core.windows.net)
 
@@ -9,56 +15,198 @@ directory="$BACKUP_FREQUENCY"
 sub_directory="`date +%Y-%m-%d-%H-%M`"
 
 
-backup() {
-    path=""
-    source=""
+get_endpoint() {
+    type=$1
+
     endpoint=""
-    case "$1" in
+    case "$type" in
         files)
-            path="$1/$directory/$sub_directory"
-            services="f"
             endpoint="file.core.windows.net"
             ;;
         
         blobs)
-            path="$1/$directory/$sub_directory"
-            services="b"
             endpoint="blob.core.windows.net"
             ;;    
     esac
-    
+
+    echo $endpoint
+}
+
+get_services() {
+    type=$1
+
+    services=""
+    case "$type" in
+        files)
+            services="f"
+            ;;
+        
+        blobs)
+            services="b"
+            ;;    
+    esac
+
+    echo $services
+}
+
+get_sas() {
+    type=$1
+    sa_name=$2
+    sa_key=$3
+
+    end=$(sas_end_date)
+    services=$(get_services $type)
+    sas=$(az storage account generate-sas --services $services --resource-types sco --account-name $sa_name --account-key $sa_key --https-only --permissions dlrw --expiry $end -otsv)
+
+    echo $sas
+}
+
+sas_end_date() {
     end=$(date -d@"$(( `date +%s`+120*60))" '+%Y-%m-%dT%H:%M:%SZ')
-    SOURCE_SAS=$(az storage account generate-sas --services $services --resource-types sco --account-key $SOURCE_STORAGE_ACCOUNT_KEY --account-name $SOURCE_STORAGE_ACCOUNT_NAME --https-only --permissions dlrw --expiry $end -otsv)
-    DESTINATION_SAS=$(az storage account generate-sas --services b --resource-types sco --account-key $DESTINATION_STORAGE_ACCOUNT_KEY --account-name $DESTINATION_STORAGE_ACCOUNT_NAME --https-only --permissions dlrw --expiry $end -otsv)
-    azcopy cp "https://$SOURCE_STORAGE_ACCOUNT_NAME.$endpoint/?$SOURCE_SAS" "https://$DESTINATION_STORAGE_ACCOUNT_NAME.blob.core.windows.net/$container/$path?$DESTINATION_SAS" --recursive=true
+
+    echo $end
+}
+
+backup() {
+    type=$1
+
+    source_endpoint=$(get_endpoint $type)
+    destination_endpoint=$(get_endpoint "blobs")
+    path="$type/$directory/$sub_directory"
+    source_sas=$(get_sas $type $SOURCE_STORAGE_ACCOUNT_NAME $SOURCE_STORAGE_ACCOUNT_KEY)
+    destination_sas=$(get_sas "blobs" $DESTINATION_STORAGE_ACCOUNT_NAME $DESTINATION_STORAGE_ACCOUNT_KEY)
+
+
+    echo -e "\e[32m--- \e[0m"
+    echo -e "\e[32m** BACKUP\e[0m"
+    echo -e "\e[32m--- \e[0m"
+    echo -e "\e[32mSource:\t\t https://$SOURCE_STORAGE_ACCOUNT_NAME.$source_endpoint/\e[0m"
+    echo -e "\e[32mDestination:\t https://$DESTINATION_STORAGE_ACCOUNT_NAME.$destination_endpoint/$container/$path\e[0m"
+    echo -e "\e[32m--- \e[0m"
+    echo ""
+
+    azcopy cp "https://$SOURCE_STORAGE_ACCOUNT_NAME.$source_endpoint/?$source_sas" "https://$DESTINATION_STORAGE_ACCOUNT_NAME.$destination_endpoint/$container/$path?$destination_sas" --recursive=true
+}
+
+sync() {
+    type=$1
+    endpoint=$(get_endpoint $type)
+    source_sas=$(get_sas $type $SOURCE_STORAGE_ACCOUNT_NAME $SOURCE_STORAGE_ACCOUNT_KEY)
+    destination_sas=$(get_sas $type $DESTINATION_STORAGE_ACCOUNT_NAME $DESTINATION_STORAGE_ACCOUNT_KEY)
+
+    echo -e "\e[32m--- \e[0m"
+    echo -e "\e[32m** SYNC\e[0m"
+    echo -e "\e[32m--- \e[0m"
+    echo -e "\e[32mSource:\t\t https://$SOURCE_STORAGE_ACCOUNT_NAME.$endpoint/\e[0m"
+    echo -e "\e[32mDestination:\t https://$DESTINATION_STORAGE_ACCOUNT_NAME.$endpoint/\e[0m"
+    echo -e "\e[32m--- \e[0m"
+    echo ""
+
+    source_containers_or_shares=""
+    if [ "$type" == "blobs" ]; then
+        source_containers_or_shares=$(az storage container list --only-show-errors -o json --account-key $SOURCE_STORAGE_ACCOUNT_KEY --account-name $SOURCE_STORAGE_ACCOUNT_NAME --num-results "*" | jq '[.[].name]')
+    elif [ "$type" == "files" ]; then
+        source_containers_or_shares=$(az storage share list --only-show-errors -o json --account-key $SOURCE_STORAGE_ACCOUNT_KEY --account-name $SOURCE_STORAGE_ACCOUNT_NAME --num-results "*" | jq '[.[].name]')
+    fi
+
+    destination_containers_or_shares=""
+    if [ "$type" == "blobs" ]; then
+        destination_containers_or_shares=$(az storage container list --only-show-errors -o json --account-key $DESTINATION_STORAGE_ACCOUNT_KEY --account-name $DESTINATION_STORAGE_ACCOUNT_NAME --num-results "*" | jq '[.[].name]')
+    elif [ "$type" == "files" ]; then
+        destination_containers_or_shares=$(az storage share list --only-show-errors -o json --account-key $DESTINATION_STORAGE_ACCOUNT_KEY --account-name $DESTINATION_STORAGE_ACCOUNT_NAME --num-results "*" | jq '[.[].name]')
+    fi
+
+    to_be_copied_containers_or_shares=$(echo $source_containers_or_shares | jq --argjson d "$destination_containers_or_shares" '. - $d')
+    to_be_removed_containers_or_shares=$(echo $destination_containers_or_shares | jq -r --argjson s "$source_containers_or_shares" '. - $s | .[]')
+    to_be_synced_containers_or_shares=$(echo $source_containers_or_shares | jq -r --argjson c "$to_be_copied_containers_or_shares" '. - $c | .[]')
+    to_be_copied_containers_or_shares=$(echo $source_containers_or_shares | jq -r --argjson d "$destination_containers_or_shares" '. - $d | .[]')
+
+    for k in "${to_be_copied_containers_or_shares[@]}"
+    do
+        if [ -z "$k" ]; then
+            break
+        fi
+        echo -e "\e[32m--- \e[0m"
+        echo -e "\e[32m** COPY\e[0m"
+        echo -e "\e[32m--- \e[0m"
+        echo -e "\e[32mSource:\t\t https://$SOURCE_STORAGE_ACCOUNT_NAME.$endpoint/${k%/}\e[0m"
+        echo -e "\e[32mDestination:\t https://$DESTINATION_STORAGE_ACCOUNT_NAME.$endpoint/${k%/}\e[0m"
+        echo -e "\e[32m--- \e[0m"
+        echo ""
+
+        azcopy cp "https://$SOURCE_STORAGE_ACCOUNT_NAME.$endpoint/${k%/}/*?$source_sas" "https://$DESTINATION_STORAGE_ACCOUNT_NAME.$endpoint/${k%/}?$destination_sas" --recursive=true
+    done
+
+    for k in "${to_be_removed_containers_or_shares[@]}"
+    do
+        if [ -z "$k" ]; then
+            break
+        fi
+        echo -e "\e[32m--- \e[0m"
+        echo -e "\e[32m** REMOVE\e[0m"
+        echo -e "\e[32m--- \e[0m"
+        echo -e "\e[32mDirectory:\thttps://$DESTINATION_STORAGE_ACCOUNT_NAME.$endpoint/${k%/} ... \e[0m"
+        echo -e "\e[32m--- \e[0m"
+        echo ""
+        azcopy rm "https://$DESTINATION_STORAGE_ACCOUNT_NAME.$endpoint/${k%/}?$destination_sas" --recursive=true
+    done
+
+    for k in "${to_be_synced_containers_or_shares[@]}"
+    do
+        if [ -z "$k" ]; then
+            break
+        fi
+        echo -e "\e[32m--- \e[0m"
+        echo -e "\e[32m** SYNC\e[0m"
+        echo -e "\e[32m--- \e[0m"
+        echo -e "\e[32mSource:\t\t https://$SOURCE_STORAGE_ACCOUNT_NAME.$endpoint/${k%/}\e[0m"
+        echo -e "\e[32mDestination:\t https://$DESTINATION_STORAGE_ACCOUNT_NAME.$endpoint/${k%/}\e[0m"
+        echo -e "\e[32m--- \e[0m"
+        echo ""
+
+        azcopy sync "https://$SOURCE_STORAGE_ACCOUNT_NAME.$endpoint/${k%/}?$source_sas" "https://$DESTINATION_STORAGE_ACCOUNT_NAME.$endpoint/${k%/}?$destination_sas" --recursive=true --delete-destination=true
+    done
 }
 
 rotate () {
+    type=$1
+    
     if [[ -z "${BACKUP_RETENTION_COUNT}" ]]; then
-        echo "retention not set, skipping backup rotation"
+        echo -e "\e[31mretention not set, skipping backup rotation\e[0m"
     else
         # tail -n +X starts from Xth line, so add 1
-        BACKUP_RETENTION_COUNT=$((BACKUP_RETENTION_COUNT+1))
-        rotate_dirs=($(az storage blob directory list --account-key $DESTINATION_STORAGE_ACCOUNT_KEY --account-name $DESTINATION_STORAGE_ACCOUNT_NAME -c $container -d "$1/$directory" -o json --delimiter '/' | jq -r 'sort_by(.name) | reverse | .[].name' | tail -n +${BACKUP_RETENTION_COUNT}))
+        keep=$((BACKUP_RETENTION_COUNT+1))
+        endpoint=$(get_endpoint "blobs")
+        destination_sas=$(get_sas "blobs" $DESTINATION_STORAGE_ACCOUNT_NAME $DESTINATION_STORAGE_ACCOUNT_KEY)
+        rotate_dirs=($(az storage blob directory list --only-show-errors --account-key $DESTINATION_STORAGE_ACCOUNT_KEY --account-name $DESTINATION_STORAGE_ACCOUNT_NAME -c $container -d "$type/$directory" -o json --delimiter '/' | jq -r 'sort_by(.name) | reverse | .[].name' | tail -n +${keep}))
+
         for k in "${rotate_dirs[@]}"
         do
-            echo "deleting ${k%/} ..."
-            az storage blob directory delete --account-key $DESTINATION_STORAGE_ACCOUNT_KEY --account-name $DESTINATION_STORAGE_ACCOUNT_NAME -c $container -d ${k%/} --recursive
+            echo -e "\e[32m--- \e[0m"
+            echo -e "\e[32m** ROTATE\e[0m"
+            echo -e "\e[32m--- \e[0m"
+            echo -e "\e[32mDirectory:\thttps://$DESTINATION_STORAGE_ACCOUNT_NAME.$endpoint/$container/${k%/} ... \e[0m"
+            echo -e "\e[32m--- \e[0m"
+            echo ""
+            azcopy rm "https://$DESTINATION_STORAGE_ACCOUNT_NAME.$endpoint/$container/${k%/}?$destination_sas" --recursive=true
         done
     fi
 }
 
-az storage container create \
-    -n $container \
-    --account-key $DESTINATION_STORAGE_ACCOUNT_KEY \
-    --account-name $DESTINATION_STORAGE_ACCOUNT_NAME 
-
-if [[ ! -z "$file_enabled" ]]; then
+if [ ! -z "$file_enabled" ] && [ "$MODE" == "FULL" ]; then
     backup "files"
     rotate "files"
 fi
 
-if [[ ! -z "$blob_enabled" ]]; then
+if [ ! -z "$file_enabled" ] && [ "$MODE" == "SYNC" ]; then
+    sync "files"
+fi
+
+if [ ! -z "$blob_enabled" ] && [ "$MODE" == "FULL" ]; then
     backup "blobs"
     rotate "blobs"
+fi
+
+if [ ! -z "$blob_enabled" ] && [ "$MODE" == "SYNC" ]; then
+    sync "blobs"
 fi
